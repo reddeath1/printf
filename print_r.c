8f660035038a9f74cf0e2aba3d9c506a5bc12518
Reddeath <frankslayer1@gmail.com>
2023-04-26 01:34:01 +0300

    Update print_r.c

--- print_r.c
+++ print_r.c
@@ -7,18 +7,15 @@
  */
 int print_r(va_list r)
 {
-	char *str;
-	int i = 0, counter = 0;
+	char *s = va_arg(r, char*);
+	int i;
+	int j = 0;
 
-	str = va_arg(r, char *);
-	if (str == NULL)
-		str = ")llun(";
-	while (str[i])
-		i++;
-	for (i -= 1; i >= 0; i--)
-	{
-		_putchar(str[i]);
-		counter++;
-	}
-	return (counter);
+	if (s == NULL)
+		s = "(null)";
+	while (s[j] != '\0')
+		j++;
+	for (i = j - 1; i >= 0; i--)
+		_putchar(s[i]);
+	return (j);
 }
