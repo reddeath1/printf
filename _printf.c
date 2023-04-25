03aee09d38a3fc5d32d8a7df57a5f8ecd96f6c4e
reddeath1 <frankslayer1@gmail.com>
2023-04-25 16:06:07 -0700

    fixing tasks errors

--- _printf.c
+++ _printf.c
@@ -1,52 +1,95 @@
 #include "main.h"
+void print_buffer(char buffer[], int *buff_ind);
 /**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
+int _printf(const char *format, ...)
+
+{
+
+    int i, printed = 0, printed_chars = 0;
+
+    int flags, width, precision, size, buff_ind = 0;
+
+    va_list list;
+
+    char buffer[BUFF_SIZE];
+
+    if (format == NULL)
+
+        return (-1);
+
+    va_start(list, format);
+
+    for (i = 0; format && format[i] != '\0'; i++)
+
+    {
+
+        if (format[i] != '%')
+
+        {
+
+            buffer[buff_ind++] = format[i];
+
+            if (buff_ind == BUFF_SIZE)
+
+                print_buffer(buffer, &buff_ind);
+
+            /* write(1, &format[i], 1);*/
+
+            printed_chars++;
+        }
+
+        else
+
+        {
+
+            print_buffer(buffer, &buff_ind);
+
+            flags = get_flags(format, &i);
+
+            width = get_width(format, &i, list);
+
+            precision = get_precision(format, &i, list);
+
+            size = get_size(format, &i);
+
+            ++i;
+
+            printed = handle_print(format, &i, list, buffer,
+
+                                   flags, width, precision, size);
+
+            if (printed == -1)
+
+                return (-1);
+
+            printed_chars += printed;
+        }
+    }
+
+    print_buffer(buffer, &buff_ind);
+
+    va_end(list);
+
+    return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
  */
 
-int _printf(const char * const format, ...)
+void print_buffer(char buffer[], int *buff_ind)
+
 {
-	convert_match m[] = {
-		{"%s", print_string}, 
-		{"%c", print_char},
-		{"%%", print_37},
-		{"%i", print_int},
-		{"%d", print_dec},
-		{"%r", reverse_string},
-		{"%R", _rot13},
-		{"%b", print_bin},
-		{"%u", _unsigned},
-		{"%o", _oct},
-		{"%x", print_hex},
-		{"%X", print_HEX},
-		{"%S", print_exclusive_string},
-		{"%p", _pointer}
-	};
 
-	va_list args;
-	int i = 0, j, len = 0;
+    if (*buff_ind > 0)
 
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
+        write(1, &buffer[0], *buff_ind);
 
-	while (format[i] != '\0')
-	{
-		j = 13;
-		while (j >= 0)
-		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len += m[j].f(args);
-				i = i + 2;
-			}
-			j--;
-		}
-		_putchar(format[i]);
-		len++;
-		i++;
-	}
-	va_end(args);
-	return (len);
+    *buff_ind = 0;
 }
